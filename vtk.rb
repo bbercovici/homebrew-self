69214aba3e91fc7723d04a2975fba71a9cc774cc
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-19 21:33:19 -0600

    updated formula

--- vtk.rb
+++ vtk.rb
@@ -123,7 +123,7 @@ class Vtk < Formula
       
 
       system "cmake", "..", *args
-      system "make -j#{N_treads}"
+      system "make -j#{build_threads}"
       system "make", "install"
     end
   end
