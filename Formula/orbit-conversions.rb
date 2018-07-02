f018701aff56c5bc04d7eb6720689bdbff4996b9
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-07-02 15:31:10 -0600

    fixed hash

--- Formula/orbit-conversions.rb
+++ Formula/orbit-conversions.rb
@@ -25,7 +25,7 @@ class OrbitConversions < Formula
   desc "A collection of orbit conversion routines"
   homepage "https://github.com/bbercovici/OrbitConversions"
   url "https://github.com/bbercovici/OrbitConversions/archive/1.0.2.tar.gz"
-  sha256 "6a029acba0943afcba4a8fd95b6f81554b98f5fd719d103b447048acbeadb616"
+  sha256 "aa37858dfc0486ec2e7e8548854654a0ca1d8e969c775707376d44689edaf972"
 
   option "without-gcc", "Will not attempt to find an OMP-compliant GCC compiler in Homebrew's Cellar"
   depends_on "cmake" => :build
