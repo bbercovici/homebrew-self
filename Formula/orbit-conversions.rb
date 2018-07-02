395b11292aff22acd534f41a3a65fcb5e1a8c52c
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-07-02 15:17:25 -0600

    updating OrbitConversions version

--- Formula/orbit-conversions.rb
+++ Formula/orbit-conversions.rb
@@ -24,7 +24,7 @@
 class OrbitConversions < Formula
   desc "A collection of orbit conversion routines"
   homepage "https://github.com/bbercovici/OrbitConversions"
-  url "https://github.com/bbercovici/OrbitConversions/archive/1.0.0.tar.gz"
+  url "https://github.com/bbercovici/OrbitConversions/archive/1.0.1.tar.gz"
   sha256 "795cf5b4c959a27ef96d2433a889e2935c9586c27e77779ec9eb9bed7dbfa426"
 
   option "without-gcc", "Will not attempt to find an OMP-compliant GCC compiler in Homebrew's Cellar"
