8d6afec604496bba2a64e169312e2bf5e279681e
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-01-31 16:44:26 -0700

    fixed shapeuqlib formula name

--- Formula/shapeuqlib.rb
+++ Formula/shapeuqlib.rb
@@ -20,10 +20,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-class ShapeUqLib < Formula
+class Shapeuqlib < Formula
   desc "Implementation of an analytical shape uncertainty and inertia statistics formulation."
-  homepage "https://github.com/bbercovici/RigidBodyKinematics"
-  url "https://github.com/bbercovici/RigidBodyKinematics/archive/1.0.0.tar.gz"
+  homepage "https://github.com/bbercovici/ShapeUQLib"
+  url "https://github.com/bbercovici/ShapeUQLib/archive/1.0.0.tar.gz"
   sha256 "84412bc2402d5d2d8edb0700794bcd59d6d5231fedf1cde58d3e12188ec3aafd"
 
   depends_on "cmake" => :build
