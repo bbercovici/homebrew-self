f9081166881a3a013883aec5f4a907e2727bc7db
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-01-31 18:39:51 -0700

    updated formulae

--- Formula/shapeuqlib.rb
+++ Formula/shapeuqlib.rb
@@ -29,18 +29,17 @@ class Shapeuqlib < Formula
   depends_on "cmake" => :build
   depends_on "armadillo"
   depends_on "libomp"
-  depends_on "gcc"
 
   def install
 
     # Compile
     system "cmake . -DBREW:BOOL=TRUE -DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include' -DOpenMP_C_FLAGS='-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include' -DOpenMP_CXX_LIB_NAMES=omp -DOpenMP_C_LIB_NAMES=omp -DOpenMP_omp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib"  
-    system "make"
+    system "make -j"
 
     # Create symlink to library
     lib.install "libShapeUQLib.dylib"
-    include.install "include/ShapeUQLib.hpp"
-    share.install "ShapeUQLib/"
+    include.install "include/ShapeUQLib"
+    share.install "share/ShapeUQLib"
 
   end
 
